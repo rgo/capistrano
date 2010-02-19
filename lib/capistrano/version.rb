c0aa2a21e8ab6eb056385c4fce81902b80d3dcda
Rafa G <rgo@aspgems.com>
2010-02-19 17:47:32 +0100

    Version numbers are integers

--- lib/capistrano/version.rb
+++ lib/capistrano/version.rb
@@ -4,7 +4,7 @@ module Capistrano
 
     CURRENT = File.read(File.dirname(__FILE__) + '/../../VERSION')
 
-    MAJOR, MINOR, TINY = CURRENT.split(".",3)
+    MAJOR, MINOR, TINY = CURRENT.split(".",3).map{|n| n.to_i}
 
     STRING = CURRENT.to_s
 
